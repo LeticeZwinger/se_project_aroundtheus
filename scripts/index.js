be4e10aae301f957867a9d8c70e295120c159909
Letice Zwinger <leticedss@gmail.com>
2024-04-23 08:03:14 -0500

    final stage first attempt

--- scripts/index.js
+++ scripts/index.js
@@ -24,61 +24,53 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-// --------- THIS WORKS ------>
+
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const editCloseButton = document.querySelector("#modal-close-button");
+const profileTitle = document.querySelector("#profile-title");
+const profileDescription = document.querySelector("#profile-description");
+const profileTitleInput = document.querySelector("#modal-input-title");
+const profileDescriptionInput = document.querySelector(
+  "#modal-input-description",
+);
+
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+function closeModal() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__closed");
-  editCloseButton.classList.remove("modal__closed");
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
-  editCloseButton.classList.add("modal__opened");
 });
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.src = cardData.link;
+  return cardElement;
+}
 
 editCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
-  editCloseButton.classList.remove("modal__opened");
-  profileEditModal.classList.add("modal__closed");
-  editCloseButton.classList.add("modal__closed");
+  closeModal();
+});
+profileEditForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
 });
 
-// -------- THIS DOES NOT WORK ---->
-
-// const profileEditButton = document.querySelector("#profile-edit-button");
-// const profileEditModal = document.querySelector("#profile-edit-modal");
-// const editCloseButton = profileEditModal.querySelector(".modal__closed");
-
-// profileEditButton.addEventListener("click", () => {
-//   profileEditModal.classList.add("modal__opened");
-// });
-// editCloseButton.addEventListener.add("click", () => {
-//   profileEditModal.classList.remove("modal__opened");
-// });
-
-// ---------PROJECT 4 FINAL STAGE------WORKING ON IT, IGNORE --------
-
-// const profileEditButton = document.querySelector("#profile-edit-button");
-// const profileEditModal = document.querySelector("#profile-edit-modal");
-// const editCloseButton = document.querySelector("#modal-close-button");
-// const profileTitle = document.querySelector("#profile-title");
-// const profileDescription = document.querySelector("#profile-description");
-// const modalTitleInput = document.querySelector("#modal-input-title");
-// const modalDescriptionInput = document.querySelector(
-//   "#modal-input-description",
-// );
-
-// profileEditButton.addEventListener("click", () => {
-//   profileEditModal.classList.remove("modal__closed");
-//   editCloseButton.classList.remove("modal__closed");
-//   profileEditModal.classList.add("modal__opened");
-//   editCloseButton.classList.add("modal__opened");
-// });
-// console.log(profileDescription.textContent);
-
-// editCloseButton.addEventListener("click", () => {
-//   profileEditModal.classList.remove("modal__opened");
-//   editCloseButton.classList.remove("modal__opened");
-//   profileEditModal.classList.add("modal__closed");
-//   editCloseButton.classList.add("modal__closed");
-// });
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
+});
